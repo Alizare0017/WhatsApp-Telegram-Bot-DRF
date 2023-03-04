8328783cf1ddb158b1e7e6e34ad92898b60a0d45
Alizare0017 <alizare0017@gmail.com>
2023-03-04 14:07:06 +0330

    fixe dome bogs

--- config/users/models.py
+++ config/users/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 class User(models.Model):
     
     username = models.CharField(max_length=32,null=False)
     userID = models.CharField(max_length=30,null=False)
-    email = models.EmailField(null=True)
-    token = models.CharField(null=True,max_length=40)
-    charge = models.IntegerField(null=True)
-    sent = models.IntegerField(null=True)
+    email = models.EmailField(null=True,default=None)
+    token = models.CharField(null=True,max_length=40,default=None)
+    charge = models.IntegerField(null=True,default=0)
+    sent = models.IntegerField(default=0)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    exp_date = models.DateTimeField(null=True)
-    last_charge_date = models.DateTimeField(null=True)
-    changed_date = models.DateTimeField(null=True)
+    exp_date = models.DateTimeField(default=timezone.now)
+    last_charge_date = models.DateTimeField(null=True,default=None)
+    changed_date = models.DateTimeField(default=timezone.now)
     change_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.userID
